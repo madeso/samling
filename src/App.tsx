d3c8529bbad80a10a6df516c4dc67dc6cd463237
Gustav <sir.gustav.the.coder@gmail.com>
2025-12-26 10:39:04 +0100

    Move AddEdit to own file

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { AddFromHtml } from './ui/AddFromHtml';
 import { AddFromLines } from './ui/AddFromLines';
 import { evaluation } from './lib/eval';
 import { is_excluded } from './lib/filter';
+import { AddEdit } from './ui/AddEdit';
 
 const DeleteIcon = () => <Icon path={mdiDelete} size={1} color="red" title={"Delete"} />;
 
@@ -101,45 +102,6 @@ const AddTagsToFiltered = (props: { eval: EvalFunction, store: Store, setStore: 
 }
 
 
-const AddEdit = (props: { index: number | null, store: Store, setStore: (store: Store) => void, onClose: () => void }) => {
-  const [item, setItem] = useState<Item>(() => props.index !== null ? structuredClone(props.store.items[props.index]) : { properties: new Map<string, string>(), tags: [] });
-
-  const setTags = (newTags: string[]) => {
-    setItem(i => ({ ...i, tags: newTags }));
-  };
-  const onOk = () => {
-    const s = structuredClone(props.store);
-    if (props.index !== null) {
-      s.items[props.index] = structuredClone(item);
-    }
-    else {
-      s.items.push(structuredClone(item));
-    }
-    props.setStore(s);
-    props.onClose();
-  };
-
-  return (
-    <Form>
-      {
-        [...item.properties.entries()].map(([key, value], prop_index) => {
-          return <Form.Group className="mb-3" key={prop_index}>
-            <Form.Label className="fw-bold">{key}</Form.Label>
-            <Form.Control type="text" value={value} onChange={ev => setItem(i => ({ ...i, properties: new Map<string, string>([...i.properties, [key, ev.target.value]]) }))} placeholder={`Enter item ${key}`} />
-          </Form.Group>
-        })
-      }
-      <Form.Group className="mb-3">
-        <Form.Label className="fw-bold">Tags</Form.Label>
-        <TagEdit tags={item.tags} setTags={setTags} />
-      </Form.Group>
-      <div className="d-flex justify-content-end gap-2 mt-3">
-        <Button variant="success" onClick={onOk}>OK</Button>
-        <Button variant="secondary" onClick={props.onClose}>Abort</Button>
-      </div>
-    </Form>
-  );
-}
 
 function App() {
   const [pattern, setPattern] = useState<string>("%name%");
